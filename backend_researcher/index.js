c163d05da310778fd9003f437c8e2c745ee315ae
BaronLiu <baronliu1993@gmail.com>
2025-10-17 19:58:29 -0400

    cors error fixes

--- backend_researcher/index.js
+++ backend_researcher/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://anjiapp.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
