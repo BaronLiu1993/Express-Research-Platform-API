7f82cd00d78344f98f4d7ead8a04f76f0225a8fb
BaronLiu <baronliu1993@gmail.com>
2025-10-17 19:29:36 -0400

    fixing backend redirect

--- backend_researcher/router/auth/authrouter.js
+++ backend_researcher/router/auth/authrouter.js
@@ -20,7 +20,7 @@ router.get("/signup-with-google", async (req, res) => {
         provider: "google",
         options: {
           redirectTo:
-            "https://t-search-test.vercel.app/account/register",
+            "https://anjiapp.vercel.app/account/register",
           scopes: scopes.join(" "),
           queryParams: {
             access_type: "offline",
@@ -48,7 +48,7 @@ router.get("/signin-with-google", async (req, res) => {
         provider: "google",
         options: {
           redirectTo:
-            "https://t-search-test.vercel.app/account/login",
+            "https://anjiapp.vercel.app/account/login",
           scopes: scopes.join(" "),
           queryParams: {
             access_type: "offline",
