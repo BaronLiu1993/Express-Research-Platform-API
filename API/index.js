d844b0be80421a3e3d560a33f7efc5aa69209c45
BaronLiu <baronliu1993@gmail.com>
2025-10-27 10:59:15 -0400

    fixing cors

--- API/index.js
+++ API/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://anjiapp.vercel.app",
+    origin: "https://paletteprod.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
