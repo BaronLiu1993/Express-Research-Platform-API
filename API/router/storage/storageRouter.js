755a625d9fdabbe3abc25fb605fa790a2c5507e7
BaronLiu <baronliu1993@gmail.com>
2025-11-12 17:14:52 -0500

    implementing getting file url endpoints

--- API/router/storage/storageRouter.js
+++ API/router/storage/storageRouter.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { verifyToken } from "../../services/authServices.js";
-import { generateUploadPresignedURL } from "../../services/storageServices.js";
+import {
+  generateGetPresignedURL,
+  generateUploadPresignedURL,
+} from "../../services/storageServices.js";
 
 const router = express.Router();
 
@@ -12,6 +15,30 @@ router.post("/generate-upload-url", verifyToken, async (req, res) => {
       userId,
       fileName,
       fileType,
+    });
+
+    const { error: insertionError } = await req.supabaseClient
+      .from("User_Profiles")
+      .update({ fileType: fileName });
+
+    if (insertionError) {
+      return res.status(400).json({ message: "Failed To Insert" });
+    }
+
+    return res.status(200).json({ urlData: presignedURLData });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/get-file-url", verifyToken, async (req, res) => {
+  const userId = req.user.sub;
+  const { fileType, fileName } = req.query;
+  try {
+    const presignedURLData = await generateGetPresignedURL({
+      userId,
+      fileType,
+      fileName,
     });
     return res.status(200).json({ urlData: presignedURLData });
   } catch (err) {
--- API/services/storageServices.js
+++ API/services/storageServices.js
@@ -17,32 +17,40 @@ export async function generateUploadPresignedURL({
 }) {
   try {
     if (!userId || !fileName || !fileType) {
-      console.warn("[generateUploadPresignedURL] ❌ Missing params:", {
-        userId,
-        fileName,
-        fileType,
-      });
       throw new Error("Missing required parameters");
     }
 
     const filePath = `${userId}/${fileType}/${fileName}.pdf`;
 
-    console.info(
-      `[generateUploadPresignedURL] 🧾 Creating signed URL for: ${filePath}`
-    );
-
     const { data, error } = await supabase.storage
-      .from("user-files")
-      .createSignedUploadUrl(filePath, { expiresIn: 60 });
+      .from("userfiles")
+      .createSignedUploadUrl(filePath);
 
     if (error) {
       throw new Error(`Supabase Storage error`);
     }
 
-
     return data;
   } catch (err) {
-
     throw new Error(`Internal Server Error: ${err.message}`);
+  }
+}
+
+export async function generateGetPresignedURL({ userId, fileType, fileName }) {
+  try {
+    if (!userId || !fileType || !fileName) {
+      throw new Error("Missing Parameters");
+    }
+
+    const { data, error } = await supabase.storage
+      .from("userfiles")
+      .createSignedUrl(`${userId}/${fileType}/${fileName}`, 60);
+    console.log(error);
+    if (error) {
+      throw new Error("Failed to Generate URL for File");
+    }
+    return data;
+  } catch {
+    throw new Error("Internal Server Error");
   }
 }
