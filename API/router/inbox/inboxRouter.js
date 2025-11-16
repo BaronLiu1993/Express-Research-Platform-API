dcd6c3908f0b02657eeb2363aa3dce5b12156c0e
BaronLiu <baronliu1993@gmail.com>
2025-11-16 16:14:27 -0500

    removed console.log in prod

--- API/router/inbox/inboxRouter.js
+++ API/router/inbox/inboxRouter.js
@@ -163,7 +163,6 @@ router.get("/get-emails-in-thread", verifyToken, async (req, res) => {
 
     return res.status(200).json({ messageArray });
   } catch (err) {
-    console.log(err);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 });
