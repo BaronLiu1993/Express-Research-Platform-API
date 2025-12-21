a908a3e797d4cba287f38ce69a9459f4b847053d
BaronLiu <baronliu1993@gmail.com>
2025-12-21 01:41:46 -0500

    observing payload

--- API/queue/send/sendWorker.js
+++ API/queue/send/sendWorker.js
@@ -6,6 +6,7 @@ export const sendWorker = new Worker(
   "send-email",
   async (job) => {
     const { userId, userEmail, userName, body, accessToken, labelId } = job.data;
+    console.log(job.data)
     console.log(`[Worker] Processing job ${job.id} for user: ${userEmail}`);
     try {
       const result = await sendSnippetEmail({
