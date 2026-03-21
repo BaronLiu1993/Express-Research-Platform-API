aba124de4791cdc3a4bbfe3403dd8c0bfdc44cfc
baronliu1993@gmail.com <baronliu1993@gmail.com>
2026-03-21 00:10:46 -0400

    change code coverage to 80%

--- API/jest.config.js
+++ API/jest.config.js
@@ -14,9 +14,9 @@ export default {
   coverageThreshold: {
     global: {
       branches: 80,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
